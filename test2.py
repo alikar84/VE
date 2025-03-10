50049858405b8aa481f366d736aad6ea6b829055
alikar844 <alikar844@gmail.com>
2025-03-10 10:52:48 +0100

    modified

--- test2.py
+++ test2.py
@@ -1,11 +1,1 @@
-while True:
-
-    x = int(input("please enter a number: "))
-
-    try:
-        print( f' this is the number{x}')
-
-    except ValueError:
-        print('this is not a number')    
-#  see changes
-y = 10
+print("hello world")
